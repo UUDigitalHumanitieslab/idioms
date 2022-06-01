407d10b4b89c311cc90e64eb9d5ef1f559a8024e
Arjan Mossel <arjanmossel@gmail.com>
2022-06-01 13:03:48 +0200

    Add first pass at handling free text search queries

--- plugins/template_vars.py
+++ plugins/template_vars.py
@@ -8,7 +8,6 @@ def build_search_idioms_sql(args):
         'Tense': 'Tense1',
         'Aspect': 'Aspect1',
         'Modality': 'Modality1',
-        'GenStructure': 'GenStructure1',
         'OpenPosition': 'OpenPosition1',
         'OpenAnimacy': 'OpenAnimacy1',
         'SpecialVerb': 'SpecialVerb1',
@@ -16,20 +15,36 @@ def build_search_idioms_sql(args):
         'Modifier': 'Modifier1',
         'PossType': 'PossType1',
         'Alienability': 'Alienability1',
+    }
+    text_parameters = {
+        'GenStructure': 'GenStructure1',
         'IdiomNotes': 'IdiomNotes1',
     }
     wheres = []
     for param in args.keys():
-        param_sql_id = list_parameters[param]
-        # Empty text inputs are always submitted, so have to be filtered out
-        param_values = [f"'{v}'" for v in args.getlist(param) if v != '']
-        if len(param_values) > 0:
-            param_values_str = ','.join(param_values)
-            wheres.append(
-                f"""EXISTS (SELECT 1 FROM strategy_data_all sda WHERE sda.strategy_id = s.strategy_id
-                    AND sda.parameter_definition_id = '{param_sql_id}'
-                    AND sda.parameter_value IN ({param_values_str})
-                    )""")
+        if param in list_parameters.keys():
+            param_sql_id = list_parameters[param]
+            # If url parameter is present there should be a value, but keep the check to be sure
+            param_values = [f"'{v}'" for v in args.getlist(param) if v != '']
+            if len(param_values) > 0:
+                param_values_str = ','.join(param_values)
+                wheres.append(
+                    f"""EXISTS (SELECT 1 FROM strategy_data_all sda WHERE sda.strategy_id = s.strategy_id
+                        AND sda.parameter_definition_id = '{param_sql_id}'
+                        AND sda.parameter_value IN ({param_values_str})
+                        )""")
+        if param in text_parameters.keys():
+            param_sql_id = text_parameters[param]
+            # Empty text inputs are always submitted, so have to be filtered out
+            text_param = args.get(param) if args.get(param) != '' else None
+            # TODO: handle: 1. Multiple values separately; 2. Quoted string,
+            # 3. AND, OR; 4. NULL / No Value.
+            if text_param:
+                wheres.append(
+                    f"""EXISTS (SELECT 1 FROM strategy_data_all sda WHERE sda.strategy_id = s.strategy_id
+                        AND sda.parameter_definition_id = '{param_sql_id}'
+                        AND sda.parameter_value LIKE '%{text_param}%'
+                        )""")
 
     wheres_str = '\n AND '.join(wheres)
 
