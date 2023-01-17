2744b324412dba0620e2a27e4571af79bed1ffa1
Arjan Mossel <arjanmossel@gmail.com>
2023-01-17 13:49:06 +0100

    Show SQLite database CSV import steps

--- scripts/create-db.sh
+++ scripts/create-db.sh
@@ -5,11 +5,19 @@ cd data
 rm -f idioms.db
 
 # Create the table and main view structures
+echo "Creating SQLite database (data/idioms.db) and table structures"
 sqlite3 idioms.db < conversion/sql-create-tables.sql
 
 # Import the table data from CSV
+echo "Importing CSV data into SQLite database "
 TABLES='valueType valueDefinition parameterGroup parameterQuestion parameterDefinition answerset answerset_data strategy strategy_data sentence sentence_data'
-for table in $TABLES ; do sqlite3 idioms.db '.mode quote' '.separator "\t"' ".import --skip 1 csv/$table.tsv $table"; done
+for table in $TABLES ; \
+    do echo "Importing data/csv/$table.tsv"; \
+    sqlite3 idioms.db '.mode quote' '.separator "\t"' ".import --skip 1 csv/$table.tsv $table"; \
+    done
 
 # Create the FTS5 virtual tables
+echo "Creating FTS5 virtual tables"
 sqlite3 idioms.db < conversion/sql-create-fts.sql
+
+echo "Finished"
